7b6271770967cc2b6fb4e3290dd8b58eb4a5c4d3
Gaël Hébert-Furoy <118003096+Vaxs02@users.noreply.github.com>
2023-11-18 10:20:41 -0500

    Update Main.java

--- src/ca/vaxs/lobby/Main.java
+++ src/ca/vaxs/lobby/Main.java
@@ -13,7 +13,7 @@ public class Main extends JavaPlugin {
 		//Create config file
 		saveDefaultConfig();
 		
-		System.out.println("[BetterHealth]: Plugin loaded...");
+		System.out.println("[Better Lobby]: Plugin loaded...");
 		getCommand("setlobby").setExecutor(new LobbyCommand(this));
 		getCommand("lobby").setExecutor(new LobbyCommand(this));
 		getServer().getPluginManager().registerEvents(new OnJoin(this), this);
